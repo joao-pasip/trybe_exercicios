7a068e8be53f25e3488642a635d11dcfb295edc3
João Paulo <joaopasip@gmail.com>
2021-11-26 20:04:53 -0300

    exercicios javascript

---
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia-1-introducao-javascript/pratica.js
@@ -0,0 +1,49 @@
+/*
+const a = 10;
+const b =  5;
+*/
+
+//ATIVIDADE 1
+
+/*let soma = a + b;
+console.log(soma);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a/b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
+*/
+
+//ATIVIDADE 2
+
+/*let maior = 0;
+if (a > b) {
+    console.log("O número ",a,"é maior que o número ",b);
+    maior = a;
+    console.log(maior);
+} else {
+    console.log("O número ",b,"é maior que o número ",a);
+    maior = b;
+    console.log(maior);
+}
+*/
+
+//ATIVIDADE 3
+const a = 10;
+const b = 30;
+const c = 20;
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
