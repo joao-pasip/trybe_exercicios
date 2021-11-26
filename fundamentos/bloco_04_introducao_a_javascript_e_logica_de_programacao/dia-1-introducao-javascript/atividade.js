8cb8b72cb610e317ad8925bb045143e9391b2592
João Paulo <joaopasip@gmail.com>
2021-11-26 13:17:11 -0300

    atividades iniciais do primeiro modulo de javascript

---
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia-1-introducao-javascript/atividade.js
@@ -0,0 +1,32 @@
+//const myName = "João Pasip";
+//const birthCity ="Bom Jesus da Lapa-BA"
+//let birthYear = 2000;
+//birthYear = 2030;
+//birthCity = "Guanambi";
+
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
+
+
+/*let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);*/
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = base + base + height + height;
+
+console.log(area);
+console.log(perimeter);
