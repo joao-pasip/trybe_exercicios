a259d69119e4f0e1365250039da8192b3f32dfb0
João Paulo <joaopasip@gmail.com>
2021-11-29 20:42:42 -0300

    finaliza exercicios

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia-2-array-e-loop-for/exercicios.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia-2-array-e-loop-for/exercicios.js
@@ -89,13 +89,14 @@ console.log(menor);
 */
 
 //ATIVIDADE 8 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
-/*
-let soma = 0;
+
+//let soma = 0;
 for (let i = 1; i <= 25; i+=1) {
-    soma = soma + i;
+//   soma = soma + i;
+    console.log(i);
 }
-console.log(soma);
-*/
+//console.log(soma);
+
 
 //ATIVIDADE 9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 
 /*
